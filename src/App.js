e2fa590455711e320516baa9661dc751752ed210
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-07-03 16:03:14 +0700

    Setup `eslint`, `prettier`; update `README`

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './assets/logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           href="https://mui.com/material-ui/getting-started/overview/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn Material UI
         </a>
       </header>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {CssBaseline, ThemeProvider} from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
