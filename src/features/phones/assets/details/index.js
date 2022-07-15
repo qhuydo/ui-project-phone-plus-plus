17a7cde27b1a6d62899c35626faf04832a6d5329
kiethoang214 <kiethoang611@gmail.com>
2022-07-15 17:04:50 +0700

    add phone detail data

--- src/features/phones/assets/details/index.js
+++ src/features/phones/assets/details/index.js
@@ -1,3 +1,25 @@
 import { default as phone0 } from "./0.json";
+import { default as phone1 } from "./1.json";
+import { default as phone2 } from "./2.json";
+import { default as phone3 } from "./3.json";
+import { default as phone4 } from "./4.json";
+import { default as phone5 } from "./5.json";
+import { default as phone6 } from "./6.json";
+import { default as phone7 } from "./7.json";
+import { default as phone8 } from "./8.json";
+import { default as phone9 } from "./9.json";
+import { default as phone10 } from "./10.json";
 
-export default [phone0];
+export default [
+  phone0,
+  phone1,
+  phone2,
+  phone3,
+  phone4,
+  phone5,
+  phone6,
+  phone7,
+  phone8,
+  phone9,
+  phone10,
+];
