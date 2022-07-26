3bee31de0c55f74e1205ca4ca852e9550c03e417
kiethoang214 <kiethoang611@gmail.com>
2022-07-26 23:41:54 +0700

    add filter options for phoneSearchResult

--- src/features/phones/api/phone-api.js
+++ src/features/phones/api/phone-api.js
@@ -1,5 +1,7 @@
 import allPhoneDetails from "features/phones/assets/details";
-
+import { value } from "lodash-es";
+import { getAvgRatings } from "../utils";
+let filterFlag = false;
 export const findPhoneByKeyword = (keyword) => {
   return new Promise((resolve) => {
     const trimmedKeyword = keyword.trim();
@@ -64,8 +66,351 @@ export const getRecommendedPhones = (fromPhoneId) => {
   });
 };
 
+export const findPhoneByFilterBrand = (phoneList, filterOptions) => {
+  let result = [];
+  filterOptions.forEach((option) => {
+    phoneList.forEach((phone) => {
+      if (phone.category === option) {
+        result.push(phone);
+      }
+    });
+  });
+
+  return result;
+};
+
+function containsObject(obj, list) {
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export const findPhoneByFilterMemory = (phoneList, filterOptions) => {
+  let result = [];
+  filterOptions.forEach((option) => {
+    if (option === "below4gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory <= 4) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from128gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory === 128) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from16gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory === 16) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from256gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory === 256) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from32gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory === 32) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from512gbOrAbove") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory >= 512) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from8gb") {
+      phoneList.forEach((phone) => {
+        phone.memory.forEach((phoneMemory) => {
+          if (phoneMemory === 8) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    }
+  });
+  return result;
+};
+
+export const findPhoneByFilterRam = (phoneList, filterOptions) => {
+  let result = [];
+  filterOptions.forEach((option) => {
+    if (option === "above16gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 16) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "below1gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam <= 1) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from12gbTo16gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 12 && phoneRam < 16) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from1gbTo2gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 1 && phoneRam < 2) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from2gbTo4gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 2 && phoneRam < 4) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from4gbTo6gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 4 && phoneRam < 6) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from8gbTo12gb") {
+      phoneList.forEach((phone) => {
+        phone.ram.forEach((phoneRam) => {
+          if (phoneRam >= 8 && phoneRam < 12) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    }
+  });
+  return result;
+};
+
+export const findPhoneByFilterRatings = (phoneList, filterOptions) => {
+  let result = [];
+  filterOptions.forEach((option) => {
+    if (option === "from3Stars") {
+      phoneList.forEach((phone) => {
+        if (getAvgRatings(phone.comments) >= 3) {
+          if (!containsObject(phone, result)) {
+            result.push(phone);
+          }
+        }
+      });
+    } else if (option === "from4Stars") {
+      phoneList.forEach((phone) => {
+        if (getAvgRatings(phone.comments) >= 4) {
+          if (!containsObject(phone, result)) {
+            result.push(phone);
+          }
+        }
+      });
+    } else if (option === "from5Stars") {
+      phoneList.forEach((phone) => {
+        if (getAvgRatings(phone.comments) >= 5) {
+          if (!containsObject(phone, result)) {
+            result.push(phone);
+          }
+        }
+      });
+    }
+  });
+  return result;
+};
+
+export const findPhoneByFilterScreenSize = (phoneList, filterOptions) => {
+  let result = [];
+  filterOptions.forEach((option) => {
+    if (option === "above7") {
+      phoneList.forEach((phone) => {
+        phone.screenSize.forEach((phoneScreenSize) => {
+          if (phoneScreenSize >= 7) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "below4") {
+      phoneList.forEach((phone) => {
+        phone.screenSize.forEach((phoneScreenSize) => {
+          if (phoneScreenSize < 4) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from4To49") {
+      phoneList.forEach((phone) => {
+        phone.screenSize.forEach((phoneScreenSize) => {
+          if (phoneScreenSize >= 4 && phoneScreenSize < 5) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from5To59") {
+      phoneList.forEach((phone) => {
+        phone.screenSize.forEach((phoneScreenSize) => {
+          if (phoneScreenSize >= 5 && phoneScreenSize < 6) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    } else if (option === "from6To69") {
+      phoneList.forEach((phone) => {
+        phone.screenSize.forEach((phoneScreenSize) => {
+          if (phoneScreenSize >= 6 && phoneScreenSize < 7) {
+            if (!containsObject(phone, result)) {
+              result.push(phone);
+            }
+          }
+        });
+      });
+    }
+  });
+  return result;
+};
+
+export const findPhoneByPriceRange = (phoneList, priceRange) => {
+  let result = [];
+  phoneList.forEach((phone) => {
+    //get phone's lowest price
+    var lowestPrice = phone.versions[0].salePrice;
+    phone.versions.forEach((ver) => {
+      if (ver.salePrice < lowestPrice) {
+        lowestPrice = ver.salePrice;
+      }
+    });
+
+    if (lowestPrice >= priceRange[0] && lowestPrice <= priceRange[1]) {
+      if (!containsObject(phone, result)) {
+        result.push(phone);
+      }
+    }
+  });
+
+  return result;
+};
+
 // TODO: complete this function
 export const findPhoneAndFilter = async (keyword, filterOptions, sortBy) => {
   await new Promise((resolve) => setTimeout(resolve, 900));
-  return await findPhoneByKeyword(keyword);
+
+  let result = await findPhoneByKeyword(keyword);
+
+  //filter
+  let resultByFilter = result;
+
+  const brandFilterOptions = Object.entries(filterOptions.brand)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key);
+  const memoryFilterOptions = Object.entries(filterOptions.memory)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key);
+  const ramFilterOptions = Object.entries(filterOptions.ram)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key);
+  const ratingsFilterOptions = Object.entries(filterOptions.ratings)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key);
+  const screenSizeFilterOptions = Object.entries(filterOptions.screenSize)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key);
+
+  if (brandFilterOptions.length > 0) {
+    resultByFilter = findPhoneByFilterBrand(resultByFilter, brandFilterOptions);
+  }
+  if (memoryFilterOptions.length > 0) {
+    resultByFilter = findPhoneByFilterMemory(
+      resultByFilter,
+      memoryFilterOptions
+    );
+  }
+  if (ramFilterOptions.length > 0) {
+    resultByFilter = findPhoneByFilterRam(resultByFilter, ramFilterOptions);
+  }
+  if (ratingsFilterOptions.length > 0) {
+    resultByFilter = findPhoneByFilterRatings(
+      resultByFilter,
+      ratingsFilterOptions
+    );
+  }
+  if (screenSizeFilterOptions.length > 0) {
+    resultByFilter = findPhoneByFilterScreenSize(
+      resultByFilter,
+      screenSizeFilterOptions
+    );
+  }
+  resultByFilter = findPhoneByPriceRange(
+    resultByFilter,
+    filterOptions.priceRange
+  );
+
+  result = resultByFilter;
+
+  return result;
 };
